ba6d8a6c27bd926454de6fc0fe22f77ca58ea4e5
ltabis <tab.rysak70@gmail.com>
2021-01-05 02:47:03 +0100

    :arrow_up: added simple methods to detect with surface the ball hit.

--- pong/scripts/Ball.gd
+++ pong/scripts/Ball.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
-export var speed = 400
+export var speed = 200
 var screen_size
 var direction = Vector2()
+
+enum CollisionType { BAR, BOUNDARY }
 
 func _ready():
 	randomize()
@@ -13,12 +15,25 @@ func _ready():
 func _process(delta):
 	compute_velocity(delta)
 
-func compute_direction():
-	pass
+func _physics_process(delta):
+	detect_boundary()
+
+func bounce(collider):
+	if collider == CollisionType.BOUNDARY:
+		print("boundary hit")
+	elif collider == CollisionType.BAR:
+		print("bar hit")
 
 func compute_velocity(delta):
 	position.x += cos(direction.x) * speed * delta
 	position.y += sin(direction.y) * speed * delta
 
-func _on_CollisionShape2D_tree_entered():
-	compute_direction()
+func detect_boundary():
+	if position.x > screen_size.x or position.x < 0:
+		bounce(CollisionType.BOUNDARY)
+	elif position.y > screen_size.y or position.y < 0:
+		bounce(CollisionType.BOUNDARY)
+
+func _on_Ball_area_entered(area):
+	if area.get_name() == "Bar":
+		bounce(CollisionType.BAR)
