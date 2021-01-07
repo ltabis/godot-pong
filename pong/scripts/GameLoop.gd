d632723ad08e4c0b38e08befbabe492d0b8ca9e4
ltabis <tab.rysak70@gmail.com>
2021-01-07 14:59:21 +0100

    :sparkles: launching the game when start button clicked.

--- pong/scripts/GameLoop.gd
+++ pong/scripts/GameLoop.gd
@@ -4,6 +4,14 @@ class_name GameLoop
 var is_running = false
 
 func _ready():
+	set_game_state(false)
+
+func _on_MainMenu_start(difficulty):
+	$IA.difficulty = difficulty
+	set_game_state(true)
+
+func set_game_state(state):
 	for child in self.get_children():
-		child.hide()
-		child.set_process(false)
+		child.show() if state else child.hide()
+		child.set_process(state)
+		is_running = state
--- pong/scripts/MainMenu.gd
+++ pong/scripts/MainMenu.gd
@@ -4,4 +4,6 @@ class_name MainMenu
 signal start(difficulty)
 
 func _on_Button_pressed():
+	for child in self.get_children():
+		child.hide()
 	emit_signal("start", IA.Difficulty.EASY)
