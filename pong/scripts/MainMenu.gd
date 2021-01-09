7645242f736e221aa9dabd734167fb08be0986a0
ltabis <tab.rysak70@gmail.com>
2021-01-09 06:18:00 +0100

    :alembic: added a quit button in the main menu.

--- pong/scripts/MainMenu.gd
+++ pong/scripts/MainMenu.gd
@@ -9,3 +9,6 @@ func _on_Button_pressed():
 	for child in self.get_children():
 		child.hide()
 	emit_signal("start", $MenuButton.get_difficulty())
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
